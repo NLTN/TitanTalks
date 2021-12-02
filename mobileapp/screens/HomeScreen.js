ec63fca34bc14da170b7d272447b5b9dc247d274
jackloague1 <55412612+jackloague1@users.noreply.github.com>
2021-12-02 02:16:43 -0800

    Update HomeScreen.js

--- mobileapp/screens/HomeScreen.js
+++ mobileapp/screens/HomeScreen.js
@@ -1,5 +1,15 @@
-import React, { Component, useEffect, useState } from 'react';
-import { Alert, Text, View, Image, ScrollView, TextInput, StyleSheet, FlatList, Button } from 'react-native';
+import React, { Component, useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+  RefreshControl,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import * as PostService from '../services/PostService';
 
@@ -107,12 +117,12 @@ const HomeScreen = ({ navigation }) => {
 	React.useEffect(() => {
 		// Function that gets post data from the database
 		async function retrievePosts() {
-			const data = await PostService.getAllPostsAsync();
-			setPosts(data);
+			const serverResponse = await PostService.getAllPostsAsync();
+			setPosts(serverResponse);
 			
-			if (data) {
+			if (serverResponse) {
 				// Display pop up of data
-				Alert.alert('Read Success', JSON.stringify(data, null, 2));
+				Alert.alert('Read Success', JSON.stringify(serverResponse, null, 2));
 			}
 		}
 		retrievePosts();
@@ -125,28 +135,164 @@ const HomeScreen = ({ navigation }) => {
 	// Need to find a way to display the posts variable in return()
 	return (
 		<View>
-			
+			<FlatList
+				data={posts.data}
+				renderItem={({item})=>
+				<View
+				style={{
+				  flex: 1,
+				  flexDirection: "column",
+				}}
+			  >
+				{/* For the user avatar and username, set flexDirection to row so they can be on same line */}
+				<View
+				  style={{
+					flexDirection: "row",
+					alignItems: "center",
+					backgroundColor: "#e6edff",
+				  }}
+				>
+				  {/* Displays user's avatar */}
+				  <Image
+					source={{ uri: item.user.avatar }}
+					style={styles.user_avatar}
+				  ></Image>
+				  {/* Displays username */}
+				  <Text
+					style={{
+					  paddingLeft: 8,
+					  fontSize: 20,
+					  fontWeight: "bold",
+					}}
+				  >
+					{item.user.nickname}
+				  </Text>
+				</View>
+				{/* Display main image attachment */}
+				<Image
+				  source={{ uri: item.files.file_location }}
+				  style={{ height: 480 }}
+				></Image>
+				<View
+				  style={{
+					flex: 1,
+					flexDirection: "column",
+					height: 130,
+				  }}
+				>
+				  {/* For like, comment and share buttons, set flexDirection to row so they can be on same line */}
+				  <View
+					style={{
+					  flexDirection: "row",
+					  backgroundColor: "white",
+					}}
+				  >
+					{/* Display like button */}
+					<TouchableOpacity>
+					  <Image
+						source={require("../assets/like-icon.png")}
+						style={{ width: 30, height: 30, marginTop: 8, marginLeft: 8 }}
+					  ></Image>
+					</TouchableOpacity>
+					{/* Display comment button */}
+					<TouchableOpacity>
+					  <Image
+						source={require("../assets/comment-icon.png")}
+						style={{ width: 30, height: 30, marginTop: 8, marginLeft: 10 }}
+					  ></Image>
+					</TouchableOpacity>
+					{/* Display share button */}
+					<TouchableOpacity>
+					  <Image
+						source={require("../assets/share-icon.png")}
+						style={{ width: 30, height: 30, marginTop: 8, marginLeft: 10 }}
+					  ></Image>
+					</TouchableOpacity>
+				  </View>
+				  {/* Displays post title and post body (post text) */}
+				  <Text style={styles.likesRow}>
+					{item.likes + " likes" /*likes row */}
+				  </Text>
+				  <View
+					style={{
+					  flex: 1,
+					  flexDirection: "row",
+					  alignItems: "center",
+					  backgroundColor: "white",
+					}}
+				  >
+					<Text
+					  style={{
+						paddingLeft: 8,
+						fontSize: 17,
+						fontWeight: "bold",
+					  }}
+					>
+					  {item.user.nickname}
+					</Text>
+					<Text style={styles.caption}>{item.content}</Text>
+				  </View>
+		
+				  {/*<Text style={styles.flatListItem}>{this.props.item.Body}</Text>
+					Have removed it now to add dynamic expansion of caption later
+				   */}
+				  <Text style={styles.more}>...more</Text>
+				</View>
+			  </View>
+				}
+			></FlatList>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#b3e5fc',
-		alignItems: 'center',
-		justifyContent: 'center',
+	  flex: 1,
+	  backgroundColor: "#b3e5fc",
+	  alignItems: "center",
+	  justifyContent: "center",
 	},
 	overlay: {
-		height: 100,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: 'gray'
+	  height: 100,
+	  alignItems: "center",
+	  justifyContent: "center",
+	  backgroundColor: "gray",
+	},
+	user_avatar: {
+	  width: 50,
+	  height: 50,
+	  borderRadius: 50,
+	  marginTop: 10,
+	  marginBottom: 10,
+	  marginLeft: 8,
+	  borderColor: "blue",
+	  borderWidth: 1.5,
 	},
 	flatListItem: {
-		padding: 10,
-		fontSize: 16,
-	}
-});
+	  padding: 5,
+	  paddingBottom: 10,
+	  fontSize: 16,
+	  backgroundColor: "white",
+	},
+	likesRow: {
+	  backgroundColor: "white",
+	  paddingLeft: 8,
+	  paddingTop: 8,
+	  fontSize: 18,
+	  fontWeight: "bold",
+	},
+	caption: {
+	  fontSize: 18,
+	  paddingLeft: 5,
+	  fontWeight: "400",
+	  backgroundColor: "white",
+	},
+	more: {
+	  fontSize: 17,
+	  paddingLeft: 8,
+	  backgroundColor: "white",
+	  color: "#6B6B6B",
+	},
+  });
 
 export default HomeScreen;
